7ed79c588985db1f669d36c83a7e251a8c5b9a5c
josep.escoda@gmail.com <josep.escoda@gmail.com>
2013-03-15 11:21:19 +0100

    PatternsStatistics acabat i verificat

--- core/src/main/java/com/vkb/alg/determine/OutlierFeatureSignaturePattern.java
+++ core/src/main/java/com/vkb/alg/determine/OutlierFeatureSignaturePattern.java
@@ -2,11 +2,18 @@ package com.vkb.alg.determine;
 
 import java.util.List;
 
+import com.vkb.model.FeatureId;
 import com.vkb.model.Signature;
 
 public class OutlierFeatureSignaturePattern {
 	public OutlierFeatureSignaturePattern( List<Signature> traces ) {
 		PatternsStatistics pS = new PatternsStatistics(traces);
+		
+		// PROVA
+		double dada = pS.getFeatureStatistic(FeatureId.POSITION_X_AVG, "Mean");
+		System.out.println("Mitja obtinguda de les mitjes de la posicio X als patterns: "+dada);
+		dada = pS.getFeatureStatistic(FeatureId.POSITION_X_AVG, "Stdev");
+		System.out.println("Stdev obtinguda de les mitjes de la posicio X als patterns: "+dada);
 	}
 	
 	public double compare( Signature trace ) {
--- core/src/main/java/com/vkb/alg/determine/PatternsStatistics.java
+++ core/src/main/java/com/vkb/alg/determine/PatternsStatistics.java
@@ -47,6 +47,18 @@ public class PatternsStatistics {
 	private Map<FeatureId, Statistics> featureStatistics = new HashMap<FeatureId, Statistics>();
 	private List<Signature> signatures;
 	
+	public List<Signature> getSignatures(){
+		return signatures;
+	}
+	
+	public Statistics getFeatureStatistics(FeatureId id){
+		return featureStatistics.get(id);
+	}
+	
+	public double getFeatureStatistic(FeatureId id, String statisticName){
+		return featureStatistics.get(id).getStatistic(statisticName);
+	}
+	
 	public PatternsStatistics (List<Signature> traces){
 		Statistics statisticValue;
 		Signature sign;
@@ -66,6 +78,7 @@ public class PatternsStatistics {
 		this.signatures = traces;
 		
 		// Recorrem tota la llista de signatures
+		// Es pot simplificar el codi a costa de fer més recorreguts sobre la llista
 		for ( int i=0; i<traces.size(); ++i ) {
 			sign = traces.get(i);
 			// Per cada signatura agafem les seves features i les ajuntem
@@ -115,18 +128,25 @@ public class PatternsStatistics {
 			relation_area.addValue(sfd.getValue());
 		}
 		
-		CreateStatisticsPositionX_AVG(position_x_avg.getMean(),position_x_avg.getStandardDeviation());
-		
-		
+		CreateStatisticsMeanStdev(FeatureId.POSITION_X_AVG, position_x_avg.getMean(),position_x_avg.getStandardDeviation());
+		CreateStatisticsMeanStdev(FeatureId.POSITION_Y_AVG, position_y_avg.getMean(),position_y_avg.getStandardDeviation());
+		CreateStatisticsMeanStdev(FeatureId.VELOCITY_X_AVG, velocity_x_avg.getMean(),velocity_x_avg.getStandardDeviation());
+		CreateStatisticsMeanStdev(FeatureId.VELOCITY_Y_AVG, velocity_y_avg.getMean(),velocity_y_avg.getStandardDeviation());
+		CreateStatisticsMeanStdev(FeatureId.ACCELERATION_X_AVG, acceleration_x_avg.getMean(),acceleration_x_avg.getStandardDeviation());
+		CreateStatisticsMeanStdev(FeatureId.ACCELERATION_Y_AVG, acceleration_y_avg.getMean(),acceleration_y_avg.getStandardDeviation());
+		CreateStatisticsMeanStdev(FeatureId.AREA_X, area_x.getMean(),area_x.getStandardDeviation());
+		CreateStatisticsMeanStdev(FeatureId.AREA_Y, area_y.getMean(),area_y.getStandardDeviation());
+		CreateStatisticsMeanStdev(FeatureId.RELATION_AREA, relation_area.getMean(),relation_area.getStandardDeviation());
 	}
 	
-	private void CreateStatisticsPositionX_AVG(double mean, double stdev){
+	private void CreateStatisticsMeanStdev(FeatureId id, double mean, double stdev){
 		Statistics st = new Statistics();
+			
 		st.setStatistic("Mean", mean);
 		st.setStatistic("Stdev", stdev);
+		featureStatistics.put(id, st);
 		
-		featureStatistics.put(FeatureId.POSITION_X_AVG, st);
-		
-		System.out.println("Afegit estadistic Position_x_avg: "+st.toString());
+		System.out.println("Afegit estadistic "+id.toString()+": "+st.toString());
 	}
+	
 }
