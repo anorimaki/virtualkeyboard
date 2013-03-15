bb3f1290922cbedf7e4290877ef20347cb77a7a2
josep.escoda@gmail.com <josep.escoda@gmail.com>
2013-03-15 09:59:44 +0100

    Actualitzacio de PatternsStatistics (Backup)

--- core/src/main/java/com/vkb/alg/determine/PatternsStatistics.java
+++ core/src/main/java/com/vkb/alg/determine/PatternsStatistics.java
@@ -36,6 +36,11 @@ public class PatternsStatistics {
 			Double aux = new Double(value);
 			statistics.put(statisticName, aux);
 		}
+		
+		@Override
+		public String toString(){
+			return statistics.toString();
+		}
 	}
 	
 	
@@ -47,8 +52,17 @@ public class PatternsStatistics {
 		Signature sign;
 		Feature f;
 		ScalarFeatureData sfd;
-		DescriptiveStatistics position_x_avg = new DescriptiveStatistics();
 		
+		DescriptiveStatistics position_x_avg = new DescriptiveStatistics();
+		DescriptiveStatistics position_y_avg = new DescriptiveStatistics();
+		DescriptiveStatistics velocity_x_avg = new DescriptiveStatistics();
+		DescriptiveStatistics velocity_y_avg = new DescriptiveStatistics();
+		DescriptiveStatistics acceleration_x_avg = new DescriptiveStatistics();
+		DescriptiveStatistics acceleration_y_avg = new DescriptiveStatistics();
+		DescriptiveStatistics area_x = new DescriptiveStatistics();
+		DescriptiveStatistics area_y = new DescriptiveStatistics();
+		DescriptiveStatistics relation_area = new DescriptiveStatistics();
+				
 		this.signatures = traces;
 		
 		// Recorrem tota la llista de signatures
@@ -59,12 +73,60 @@ public class PatternsStatistics {
 			// Cal revisar si te el feature
 			sfd = f.getData();
 			position_x_avg.addValue(sfd.getValue());
-			System.out.println("Signatura #"+i+", X_AVG: "+sfd.getValue());
+			
+			f=sign.getFeature(FeatureId.POSITION_Y_AVG);
+			// Cal revisar si te el feature
+			sfd = f.getData();
+			position_y_avg.addValue(sfd.getValue());
+			
+			f=sign.getFeature(FeatureId.VELOCITY_X_AVG);
+			// Cal revisar si te el feature
+			sfd = f.getData();
+			velocity_x_avg.addValue(sfd.getValue());
+			
+			f=sign.getFeature(FeatureId.VELOCITY_Y_AVG);
+			// Cal revisar si te el feature
+			sfd = f.getData();
+			velocity_y_avg.addValue(sfd.getValue());
+
+			f=sign.getFeature(FeatureId.ACCELERATION_X_AVG);
+			// Cal revisar si te el feature
+			sfd = f.getData();
+			acceleration_x_avg.addValue(sfd.getValue());
+			
+			f=sign.getFeature(FeatureId.ACCELERATION_Y_AVG);
+			// Cal revisar si te el feature
+			sfd = f.getData();
+			acceleration_y_avg.addValue(sfd.getValue());
+			
+			f=sign.getFeature(FeatureId.AREA_X);
+			// Cal revisar si te el feature
+			sfd = f.getData();
+			area_x.addValue(sfd.getValue());
+			
+			f=sign.getFeature(FeatureId.AREA_Y);
+			// Cal revisar si te el feature
+			sfd = f.getData();
+			area_y.addValue(sfd.getValue());
+			
+			f=sign.getFeature(FeatureId.RELATION_AREA);
+			// Cal revisar si te el feature
+			sfd = f.getData();
+			relation_area.addValue(sfd.getValue());
 		}
 		
-		double data=position_x_avg.getMean();
-		System.out.println("Posicio_x_avg: "+data);
+		CreateStatisticsPositionX_AVG(position_x_avg.getMean(),position_x_avg.getStandardDeviation());
+		
 		
 	}
 	
+	private void CreateStatisticsPositionX_AVG(double mean, double stdev){
+		Statistics st = new Statistics();
+		st.setStatistic("Mean", mean);
+		st.setStatistic("Stdev", stdev);
+		
+		featureStatistics.put(FeatureId.POSITION_X_AVG, st);
+		
+		System.out.println("Afegit estadistic Position_x_avg: "+st.toString());
+	}
 }
